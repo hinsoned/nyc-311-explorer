c1ac3be16e95c33c91c186dedc0386863ea932cf
hinsoned <102074110+hinsoned@users.noreply.github.com>
2025-10-01 13:56:30 -0400

    API and column cleaning functions
    - The logic associated with the API call was given its own function
    
    - The logic associated with removing unnecessary columns was given its own function

--- app.py
+++ app.py
@@ -40,6 +40,27 @@ def prompt_user():
             print("Invalid borough. Please enter a borough.")
     return borough, start_year, end_year
 
+#function to get results from the API
+def get_results(client, DATASET_ID, LIMIT, borough, start_year, end_year):
+    all_results = []
+    #Adding 1 to the end year to include the year in the range
+    for year in range(start_year, end_year + 1):
+        start = f"{year}-01-01"
+        end = f"{year}-12-31"
+        print(f"Fetching data from {DATASET_ID} with limit {LIMIT} for {borough} in {year}...")
+        results = client.get(DATASET_ID, 
+                            limit=LIMIT,
+                            where=f"agency = 'NYPD' AND borough = '{borough}' AND created_date BETWEEN '{start}' AND '{end}'"
+                            )
+        all_results.extend(results)
+        print(f"Found {len(results)} records for {year}")
+
+    #total number of records
+    total_records = len(all_results)
+    print(f"Total number of records: {total_records}")
+
+    return all_results, total_records
+
 def open_image(path):
     system = platform.system()#This gets the operating system
     if system == "Darwin":  # macOS
@@ -88,52 +109,8 @@ def plot_seasonality_heatmap(results_df, borough, start_year, end_year, export_f
     open_image(f"{export_folder}/{borough}_seasonality_heatmap.png")
     plt.close()
 
-def main():
-    pd.set_option("display.max_columns", None)
-    pd.set_option("display.max_rows", 100)
-    pd.set_option("display.width", 1000)
-
-    APP_TOKEN = "DjO84VZPdzu9XsGU9WNTUb98F"
-    #Setting a longer timeout than the default makes the request more reliable
-    TIMEOUT_TIME = 120
-
-    client  = Socrata("data.cityofnewyork.us", APP_TOKEN, timeout=TIMEOUT_TIME)
-
-    DATASET_ID = "erm2-nwe9"
-    #The limit to the number of records I ask for
-    LIMIT = 1000000 #1 million is a limit that seems to run reliably but is still a large number of records
-
-    #Prompt user for borough, start year, and end year
-    borough, start_year, end_year = prompt_user()
-
-    #First I want to get data for a 4 year period. I wll do it in chunks of one year to avoid timeouts but this can still take a while.
-    #Be Patient!
-    #To control the number of records, I only want complaints to the NYPD in Manhattan.
-    print("Be Patient! This may take a while...")
-    print(f"Fetching data from {DATASET_ID} for {borough} between {start_year} and {end_year}...")
-    all_results = []
-    #Adding 1 to the end year to include the year in the range
-    for year in range(start_year, end_year + 1):
-        start = f"{year}-01-01"
-        end = f"{year}-12-31"
-        print(f"Fetching data from {DATASET_ID} with limit {LIMIT} for {borough} in {year}...")
-        results = client.get(DATASET_ID, 
-                            limit=LIMIT,
-                            where=f"agency = 'NYPD' AND borough = '{borough}' AND created_date BETWEEN '{start}' AND '{end}'"
-                            )
-        all_results.extend(results)
-        print(f"Found {len(results)} records for {year}")
-
-    #total number of records
-    total_records = len(all_results)
-    print(f"Total number of records: {total_records}")
-
-    #Now I convert the results to a dataframe
-    results_df = pd.DataFrame.from_records(all_results)
-
-    #I next want to check column names and data types
-    print(results_df.dtypes)
-
+#function to remove unnecessary columns
+def remove_unnecessary_columns(results_df):
     #I do not need all of these columns. I know the agency is the NYPD and the borough is Manhattan. I also do not need the bbl and the agency_name column
     #has bad information according to the documentation. Exact location is not needed for my analysis.
     #Columns to keep:
@@ -162,6 +139,43 @@ def main():
     print("Dropped columns:")
     for col in drop_columns:
         print(f"- {col}")
+
+    return results_df
+
+def main():
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_rows", 100)
+    pd.set_option("display.width", 1000)
+
+    APP_TOKEN = "DjO84VZPdzu9XsGU9WNTUb98F"
+    #Setting a longer timeout than the default makes the request more reliable
+    TIMEOUT_TIME = 120
+
+    client  = Socrata("data.cityofnewyork.us", APP_TOKEN, timeout=TIMEOUT_TIME)
+
+    DATASET_ID = "erm2-nwe9"
+    #The limit to the number of records I ask for
+    LIMIT = 1000000 #1 million is a limit that seems to run reliably but is still a large number of records
+
+    #Prompt user for borough, start year, and end year
+    borough, start_year, end_year = prompt_user()
+
+    #First I want to get data for a 4 year period. I wll do it in chunks of one year to avoid timeouts but this can still take a while.
+    #Be Patient!
+    #To control the number of records, I only want complaints to the NYPD in Manhattan.
+    print("Be Patient! This may take a while...")
+    print(f"Fetching data from {DATASET_ID} for {borough} between {start_year} and {end_year}...")
+
+    all_results, total_records = get_results(client, DATASET_ID, LIMIT, borough, start_year, end_year)
+
+    #Now I convert the results to a dataframe
+    results_df = pd.DataFrame.from_records(all_results)
+
+    #I next want to check column names and data types
+    print(results_df.dtypes)
+
+    #remove unnecessary columns
+    results_df = remove_unnecessary_columns(results_df)
 
     #Print to see the data types again
     print(results_df.dtypes)
